81b2e945f97cb5cb43db7b596f3f0c37bd54c1e0
Henry <193541T@mymail.nyp.edu.sg>
2020-10-31 08:08:06 +0800

    Added some spaces in GlobalFuncs.cpp and added field initialization to GlobalStructs.h

--- Engine/Src/Global/GlobalFuncs.cpp
+++ Engine/Src/Global/GlobalFuncs.cpp
@@ -105,12 +105,10 @@ bool InitConsole(){
 void SetUpTex(const SetUpTexsParams& params, uint& texRefID){
     stbi_set_flip_vertically_on_load(params.flipTex); //OpenGL reads y/v tex coord in reverse so must flip tex vertically
 
-
     glGenTextures(1, &texRefID);
     int currTexRefID;
     glGetIntegerv(GL_TEXTURE_BINDING_2D, &currTexRefID);
     glBindTexture(params.texTarget, texRefID); //Make tex referenced by 'texRefIDs[i]' the tex currently bound to the currently active tex unit so subsequent tex commands will config it
-
 
     int width, height, colourChannelsAmt;
     unsigned char* data = stbi_load(params.texPath.c_str(), &width, &height, &colourChannelsAmt, 0);
@@ -120,14 +118,12 @@ void SetUpTex(const SetUpTexsParams& params, uint& texRefID){
     glTexImage2D(params.texTarget, 0, colourChannelsAmt == 3 ? GL_RGB : GL_RGBA, width, height, 0, colourChannelsAmt == 3 ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, data);
     stbi_image_free(data); //Free the img mem
 
-
     glGenerateMipmap(params.texTarget); //Gen required mipmap lvls for currently bound tex
     glTexParameteri(params.texTarget, GL_TEXTURE_WRAP_S, params.texWrapParam);
     glTexParameteri(params.texTarget, GL_TEXTURE_WRAP_T, params.texWrapParam);
     glTexParameteri(params.texTarget, GL_TEXTURE_MIN_FILTER, params.texFilterMin); //Nearest neighbour/Point filtering/interpolation when textures are scaled downwards
     glTexParameteri(params.texTarget, GL_TEXTURE_MAG_FILTER, params.texFilterMag); //Linear filtering/interpolation for upscaled textures
     glBindTexture(params.texTarget, currTexRefID); //Bind previously bound tex
-
 
     stbi_set_flip_vertically_on_load(false);
 }
--- Engine/Src/Global/GlobalStructs.h
+++ Engine/Src/Global/GlobalStructs.h
@@ -2,10 +2,10 @@
 #include "../Core.h"
 
 struct SetUpTexsParams final{
-    str texPath;
-    bool flipTex;
-    int texTarget;
-    int texWrapParam;
-    int texFilterMin;
-    int texFilterMag;
+    str texPath = str();
+    bool flipTex = false;
+    int texTarget = 0;
+    int texWrapParam = 0;
+    int texFilterMin = 0;
+    int texFilterMag = 0;
 };
